ea577157d32d433abcef7aac3fbcffded54bc93b
gevannmullins <gevann.mullins@gmail.com>
2021-10-28 13:45:59 +0200

    Ready to start App

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+
+function App() {
+    return (
+        <div className="p-4">
+            <div className="container">
+                <h1>NEXTJS</h1>
+                <p>Your first app</p>
+            </div>
+        </div>
+    );
+}
+
+export default App;
