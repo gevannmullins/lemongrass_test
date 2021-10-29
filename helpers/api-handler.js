b9f4a899da545a3d64a49112532de5023596dca1
gevannmullins <gevann.mullins@gmail.com>
2021-10-29 10:08:43 +0200

    - Added an error handler in helpers. - Added the api handler to help with the api requests - Added the jwt helper for api authentication - fixed the fs error in the db helper

---
+++ helpers/api-handler.js
@@ -0,0 +1,24 @@
+import { errorHandler, jwt } from 'helpers';
+
+export { apiHandler };
+
+function apiHandler(handler) {
+    return async (req, res) => {
+        const method = req.method.toLowerCase();
+
+        // check handler supports HTTP method
+        if (!handler[method])
+            return res.status(405).end(`Method ${req.method} Not Allowed`);
+
+        try {
+            // global middleware
+            await jwt(req, res);
+
+            // route handler
+            await handler[method](req, res);
+        } catch (err) {
+            // global error handler
+            errorHandler(err, res);
+        }
+    }
+}
--- helpers/index.js
+++ helpers/index.js
@@ -1,1 +1,6 @@
 export * from './http-wrapper';
+export * from './api-handler';
+export * from './error-handler';
+export * from './jwt';
+export * from './omit';
+// export * from './db';
